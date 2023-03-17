a980cd9a30ec918c03fcef640fd87f67113c53a8
VukMan00 <vukman619@gmail.com>
2023-03-17 14:48:41 +0100

    Kreiranje klase Magacin koja implementira SpecifikacijaMagacina interfejs

---
+++ Magacin/src/specifikacijamagacina/Magacin.java
@@ -0,0 +1,25 @@
+package specifikacijamagacina;
+
+import magacin.Artikal;
+
+public class Magacin implements SpecifikacijaMagacina{
+
+	@Override
+	public void addArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getArtikal(String sifra) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
